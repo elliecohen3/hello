4014d414452e98bab4a2fbc918022b85e38d0eec
maxpoore <142830526+maxpoore@users.noreply.github.com>
2023-08-24 13:37:19 -0500

    added partner code

--- hello.c
+++ hello.c
@@ -9,5 +9,8 @@ int main(int argc, char**argv) {
 printf("Hello I'm Ellie Cohen!");
 printf("Major is Computer Science!!");
 
+printf("Hello, I'm Max Poore!");
+printf("My major is Comptuer Science!");
+
 return 0;
 }
